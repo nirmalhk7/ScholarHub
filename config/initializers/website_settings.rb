0b400a05e4ff52f5d988bcf5d3178ec51afe64d4
Nirmal Khedkar <nirmalhk7@gmail.com>
2019-08-23 20:21:54 +0530

    Signup prep

---
+++ config/initializers/website_settings.rb
@@ -0,0 +1,8 @@
+WEB_DEFAULTS = {
+    :website_name => 'ScholarHub',
+    :website_full => 'Research Portal for NIT',
+    :website_description => 'To Connect Aspiring Students with best-in-class researchers in NIT',
+    :year => Date.today.year,
+}
+
+
